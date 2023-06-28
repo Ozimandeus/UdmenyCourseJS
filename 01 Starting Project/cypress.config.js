e359beefa6b9155d26a30039760b2c154c4ca3cb
Oli Sawtell <31514204+Ozimandeus@users.noreply.github.com>
2023-06-28 15:29:29 +0100

    end of section

--- 01 Starting Project/cypress.config.js
+++ 01 Starting Project/cypress.config.js
@@ -5,6 +5,14 @@ export default defineConfig({
     baseUrl:'http://localhost:5173',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        seedDatabase(filename) {
+          //Run you NodeJS code
+          // e.g. edit a file here
+          // This runs outside of the browser.
+          return filename; 
+        }
+      });
     },
   },
 });
